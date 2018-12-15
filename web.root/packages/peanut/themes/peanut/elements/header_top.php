3f85940a719a430a4bf2aa47edd9b37cec3f6e62
Terry SoRelle <tls@2quakers.net>
2018-12-15 12:12:45 -0600

    Moved service messages to header_top.php

--- web.root/packages/peanut/themes/peanut/elements/header_top.php
+++ web.root/packages/peanut/themes/peanut/elements/header_top.php
@@ -1,4 +1,5 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
+<!-- Modified to support service messages component.  Terry SoRelle 2018-12-14 -->
 <!DOCTYPE html>
 <html lang="<?php  echo Localization::activeLanguage()?>">
 <head>
@@ -18,7 +19,39 @@
             document.querySelector('head').appendChild(msViewportStyle)
         }
     </script>
+
+    <!-- Styling for peanut service messages -->
+    <style>
+        #error-messages {
+            border: 1px solid darkred;
+            margin-top: 10px;
+        }
+        #info-message {
+            border: 1px solid darkgreen;
+            margin-top: 10px;
+        }
+        #warning-messages {
+            border: 1px solid darkblue;
+            margin-top: 10px;
+        }
+        #peanut-messages {
+            position:fixed;top:0;
+            z-index: 16777271;
+            width:100%
+        }
+
+    </style>
 </head>
 <body>
+
+<!-- Peanut service messages container -->
+<div class="container" id="peanut-messages">
+    <div class="row">
+        <div class="col-md-12"   >
+            <div id="service-messages-container"><service-messages></service-messages></div>
+        </div>
+    </div>
+</div>;
+
 
 <div class="<?php  echo $c->getPageWrapperClass()?>">
